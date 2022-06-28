ed45460f2beea0daa298085a44bb4c94e2b8726d
Blinov Vitaly <Blinov.V.A@mail.ru>
2022-06-29 03:51:02 +0400

    Реализация с общей ошибкой на batch-е т.е. без частичного смещения ассетов для разных партиций

--- SomeService/Program.cs
+++ SomeService/Program.cs
@@ -1,3 +1,187 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Collections.Immutable;
+using System.Text.Json;
+using Confluent.Kafka;
+using StackExchange.Redis;
 
-Console.WriteLine("Hello, World!");
+namespace SomeService;
+
+public static class Program
+{
+    private const string RedisHost = "localhost";
+    private const string KafkaHost = "localhost:9092";
+    private const string PersonTopic = "person_event";
+    private static  readonly ProducerConfig ProducerConfig = new()
+    {
+        BootstrapServers = KafkaHost
+    };
+    private static readonly ConsumerConfig ConsumerConfig = new()
+    {
+        BootstrapServers = KafkaHost, 
+        GroupId = "Group_2", 
+        AutoOffsetReset = AutoOffsetReset.Earliest, 
+        EnableAutoCommit = false
+    };
+
+    private static readonly TimeSpan ProducerDelay = TimeSpan.FromMilliseconds(300);
+    
+    private static readonly int MessagesBatchMaxSize = 6;
+    private static readonly TimeSpan LingerMs = TimeSpan.FromSeconds(10);
+
+    private static readonly TimeSpan ConsumerTimeout = TimeSpan.FromMilliseconds(100);
+    private static readonly TimeSpan FailDelay = TimeSpan.FromMilliseconds(500);
+
+    private static readonly Random Random = new();
+    
+    
+    private class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    private class CustomKafkaSerializer<T> : ISerializer<T>, IDeserializer<T>
+    {
+        public T Deserialize(ReadOnlySpan<byte> data, bool isNull, SerializationContext context) 
+            => JsonSerializer.Deserialize<T>(data);
+
+        public byte[] Serialize(T data, SerializationContext context) 
+            => JsonSerializer.SerializeToUtf8Bytes(data);
+    }
+
+    
+    public static async Task Main()
+    {
+        var cancellationTokenSource = new CancellationTokenSource();
+        var cancellationToken = cancellationTokenSource.Token;
+
+        // await ProduceMessages(cancellationToken);
+        
+        var consumer1Task = Task.Run(async () => await ConsumeMessages(1, cancellationToken));
+
+        await consumer1Task;
+        Console.ReadLine();
+    }
+
+    public static async Task ProduceMessages(CancellationToken cancellationToken)
+    {
+        var rand = new Random();
+        
+        using var producer = new ProducerBuilder<string, Person>(ProducerConfig)
+                             .SetValueSerializer(new CustomKafkaSerializer<Person>())
+                             .Build();
+
+        try
+        {
+            var i = 0;
+            while(!cancellationToken.IsCancellationRequested && i <= 1000)
+            {
+                var msg = new Message<string, Person>
+                {
+                    Key = i.ToString(), 
+                    Value = new Person
+                    {
+                        Name = Guid.NewGuid().ToString(), 
+                        Age = rand.Next(20, 50)
+                    }
+                };
+                
+                producer.Produce(PersonTopic, msg);
+                Console.WriteLine($"[Info] : Print ---> message: {{ (key: { msg.Key }, Value:  {{ Name: { msg.Value.Name.ToString()}), Age: { msg.Value.Age }}} }}");
+
+                i++;
+                await Task.Delay(ProducerDelay, cancellationToken);
+            }
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"[Error] : {ex.Message}");
+        }
+        finally
+        {
+            producer.Flush(cancellationToken);
+        }
+    }
+    
+    private static async Task ConsumeMessages(int consumerNumber, CancellationToken cancellationToken)
+    {
+        var redis = await ConnectionMultiplexer.ConnectAsync(RedisHost);
+        var redisDb = redis.GetDatabase();
+        
+        using var consumer = new ConsumerBuilder<string, Person>(ConsumerConfig)
+                       .SetValueDeserializer(new CustomKafkaSerializer<Person>())
+                       .Build();
+
+        consumer.Subscribe(PersonTopic);
+
+        var lastProcessedIsSuccessfully = true;
+        Queue<(TopicPartitionOffset topicPartitionOffset, Message<string, Person> message)> messages = new();
+        var deadline = DateTime.UtcNow.Add(LingerMs);
+            
+        while(!cancellationToken.IsCancellationRequested)
+        {
+            if(DateTime.UtcNow >= deadline && messages.Any() || messages.Count >= MessagesBatchMaxSize)
+            {
+                try
+                {
+                    ProcessMessage(messages.ToImmutableArray(), redisDb);
+                
+                    consumer.Commit(messages.Select(p => p.topicPartitionOffset));
+                        
+                    messages.Clear();
+                    deadline = DateTime.UtcNow.Add(LingerMs);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine($"[Error] ::: Batch processing message failure. BatchMessageFirst:{messages.First().topicPartitionOffset}, BatchMessageLast:{messages.Last().topicPartitionOffset}. Error: {ex.Message}");
+                    await Task.Delay(FailDelay);
+                    continue;
+                }
+            }
+            
+            ConsumeResult<string, Person> message = null;
+            if (lastProcessedIsSuccessfully) 
+                message = consumer.Consume(ConsumerTimeout);
+
+            try
+            {
+                if(message != null)
+                {
+                    messages.Enqueue((message.TopicPartitionOffset, message.Message));
+                    
+                    Console.WriteLine($"[Info] --> Consume {message.TopicPartitionOffset} => key : {message.Message.Key}, value: {{ {message.Message.Value.Name} : {message.Message.Value.Age} }} => InBatchPosition: {messages.Count}");
+
+                    lastProcessedIsSuccessfully = true;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"[Error] ::: consume messages error ({message.Topic}:{message.Partition}:{message.Offset}): {ex.Message}");
+                lastProcessedIsSuccessfully = false;
+            }
+
+            // Для разработки
+            await Task.Delay(TimeSpan.FromMilliseconds(500), cancellationToken);
+        }
+        
+        consumer.Close();
+    }
+
+    private static void ProcessMessage<TKey, TValue>(IReadOnlyCollection<(TopicPartitionOffset, Message<TKey, TValue>)> messages, IDatabase redis)
+    {
+        foreach(var message in messages)
+        {
+            var redisValue = redis.StringGet(message.Item1.ToString());
+            if(!redisValue.IsNullOrEmpty)
+            {
+                Console.WriteLine($"[INFO] --> redis already has TopicPartitionOffset: {message.Item1} => this message is processed already.");
+                continue;
+            }
+
+            // Processing logic
+            Console.WriteLine($"[Info] --> process {message.Item1.ToString()}");
+            if(Random.Next(10) % 9 == 0)
+                throw new Exception("Random custom error");
+
+            redis.StringSet(key: message.Item1.ToString(), value: true);
+        }
+    }
+}
